f46a6529be52533ef794ab65826c74cadcd3fe4b
Asen Dulakshana Kavinda <s22010362@ousl.lk>
2025-11-19 22:29:09 +0530

    improvemt for pdf loader

--- job_recommender/src/loaders/pdf_loader.py
+++ job_recommender/src/loaders/pdf_loader.py
@@ -9,49 +9,63 @@ from job_recommender.src.core.logger_config import logger as log
 from job_recommender.src.core.exceptions_config import ProjectException
 
 class PdfLoader:
-    def __init__(self):
-        ...
+    def __init__(self, uploaded_file: BinaryIO):
+        self.uploaded_file = uploaded_file
+        self._ensure_size()
+        self._ensure_readability()
+        
 
-    def text_extractor(self, uploaded_file: BinaryIO):
+    def text_extractor(self):
         """
         Extracts text from a PDF file-like object.
 
         Args:
-            uploaded_file (BinaryIO): A readable file-like object (e.g., BytesIO or uploaded file stream).
+            self.uploaded_file (BinaryIO): A readable file-like object (e.g., BytesIO or uploaded file stream).
 
         Returns:
             str: The extracted text from all pages.
 
         Raises:
-            ProjectExceptions
+            ProjectException: If extraction fails or input in invalid.
         """
 
 
         try:
-            with fitz.open(stream=uploaded_file.read(), filetype="pdf") as doc:
+            pdf_content = self.uploaded_file.read()
+            with fitz.open(stream=pdf_content, filetype="pdf") as doc:
                 if doc.page_count == 0:
                     log.warning("Empty PDF document")
                     return ""
+                page_texts: List[str] = []
+                for page_num, page in enumerate(doc, start=1):
+                    try:
+                        page_text = page.get_text()
+                        page_texts.append(page_text)
+                        log.debug(f"Extracted text from page {page_num}")
+                    except Exception as e:
+                        log.error(f"Faild to extract text from {page_num}")
+                        ProjectException(
+                            e,
+                            context = {
+                                "opetation": "text extractor page reader",
+                                "page number": page_num
+                            }
+                        )
                 
-                if self._ensure_size(uploaded_file) and self._ensure_readability(uploaded_file):
-                    page_texts: List[str] = []
-                    for page_num, page in enumerate(doc, start=1):
-                        try:
-                            page_text = page.get_text()
-                            page_texts.append(page_text)
-                            log.debug(f"Extracted text from page {page_num}")
-                        except Exception as e:
-                            log.error(f"Faild to extract text from {page_num}")
-                            ProjectException(
-                                e,
-                                context = {
-                                    "opetation": "text extractor page reader"
-                                }
-                            )
-                
-                    full_text = "\n".join(page_texts) # join once
-                    log.info(f"Extracted text from { doc.page_count} pages, (total chars: {len(full_text)})")
-                    return full_text
+                full_text = "\n".join(page_texts) # join once
+                log.info(f"Extracted text from { doc.page_count} pages, (total chars: {len(full_text)})")
+                return full_text
+            
+        except fitz.FileDataError as e:
+            log.error("Invalid or corrupted PDF file")
+            ProjectException(
+                e,
+                context={
+                    "operation": "text extractor",
+                    "message": "Invalid PDF file"
+                }
+            )
+                    
         except Exception as e:
             log.error(f"Unexpected error during PDF extraction")    
             ProjectException(
@@ -65,7 +79,7 @@ class PdfLoader:
 
     
 
-    def _ensure_readability(self, uploaded_file: BinaryIO) -> bool:
+    def _ensure_readability(self) -> bool:
         """ 
         Check the readabiity of the uploaded file, it should be a file-like object.
 
@@ -80,7 +94,7 @@ class PdfLoader:
         """
 
 
-        if not hasattr(uploaded_file, 'read') or not callable(uploaded_file.read):
+        if not hasattr(self.uploaded_file, 'read') or not callable(self.uploaded_file.read):
             log.warning("File not readable.")
             ProjectException(
                 "Uploaded file must be a readable file-like object.",
@@ -93,7 +107,7 @@ class PdfLoader:
             log.info("File readable.")
             return True
 
-    def _ensure_size(self, uploaded_file: BinaryIO) -> bool:
+    def _ensure_size(self) -> bool:
         """ 
         Check the size of the uploaded file,
 
@@ -108,10 +122,11 @@ class PdfLoader:
         """
 
         try:
-            file_size = uploaded_file.seek(0, 2) # get the size
-            uploaded_file.seek(0) # reset position
+            original_pos = self.uploaded_file.tell() # save current position
+            file_size = self.uploaded_file.seek(0, 2) # get the size
+            self.uploaded_file.seek(0) # reset position
             if file_size > 10 * 1024 * 1024: # 10MB limit
-                log.warning(f"File size {file_size}MB.")
+                log.warning(f"File size {file_size / (1024 * 1024):.2f}MB.")
                 ProjectException(
                     "File too large, Maximum 10MB",
                     context = {
@@ -121,7 +136,7 @@ class PdfLoader:
                 )
                 return False
             else:
-                log.info(f"File size: {file_size}, under the limit.")
+                log.info(f"File size: {file_size / (1024 * 1024):.2f}MB, under the limit.")
                 return True
         except Exception as e:
             ProjectException(
