54bdbd12e2ecbe7aba1bd7f62519515ec2f7049d
AsenDulakshanakavinda <s22010362@ousl.lk>
2026-02-02 15:43:20 +0530

    added simple missing skills extractor node and prompt

--- rec_system/prompts/__init__.py
+++ rec_system/prompts/__init__.py
@@ -1,5 +1,7 @@
 from .summarizer_node_prompt import load_summarizer_prompt
+from .missing_skills_extractor_node_prompt import load_missing_skills_extractor_prompt
 
 __all__ = [
     "load_summarizer_prompt"
+    "load_missing_skills_extractor_prompt"
 ]
